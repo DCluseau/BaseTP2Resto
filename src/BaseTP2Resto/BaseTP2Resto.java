b6bdb7765c120fff83981601c60adb98fe3e6f90
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-10-31 13:16:14 +0100

    Init project

---
+++ src/BaseTP2Resto/BaseTP2Resto.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package BaseTP2Resto;
+
+/**
+ * 
+ */
+public class BaseTP2Resto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
